2a58be383bb86dfa63bb6231c9985b7af0055c79
Alexander Hill <ahill@shopatron.com>
2013-09-06 09:18:34 -0700

    In which I help deal with our dumb custom CSVs.

---
+++ transmute-csv.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#
+# Acting as a filter, transmute CSVs with one delimiter to CSVs with another
+# delimiter.
+#
+# TODO: operate on files given on the command line.
+
+import argparse
+from sys import stdin, stdout
+import csv
+
+aparse = argparse.ArgumentParser()
+aparse.add_argument('-i', '--input',
+	help='The delimiter used by the input CSV. Defaults to ",".',
+	default=',')
+aparse.add_argument('-o', '--output',
+	help='The delimiter to be used by the output CSV. Defaults to "|".',
+	default='|')
+
+args = aparse.parse_args()
+
+csv.writer(stdout, delimiter=args.input).writerows(csv.reader(stdin, delimiter=args.output))
