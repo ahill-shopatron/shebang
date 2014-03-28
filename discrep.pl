290c8b1fc42c2d7c0ff124ae5c80d680d55d3db4
Alexander Hill <ahill@shopatron.com>
2014-03-28 10:00:53 -0700

    In which coding standards evolve

--- discrep.pl
+++ discrep.pl
@@ -9,20 +9,20 @@ my $actual = undef;
 my $table = undef;
 
 while (<>) {
-	if (/^Site (\D+?) \d/) {
-		$site = $1;
-		$expected = undef;
-		$actual = undef;
-		$table = undef;
-	} elsif (/^Found (\d+) records in table (.*)$/) {
-		$expected = $1;
-		$table = $2;
-		$actual = undef;
-	} elsif (/^Published (\d+) records to/) {
-		$actual = $1;
-	}
+    if (/^Site (\D+?) \d/) {
+        $site = $1;
+        $expected = undef;
+        $actual = undef;
+        $table = undef;
+    } elsif (/^Found (\d+) records in table (.*)$/) {
+        $expected = $1;
+        $table = $2;
+        $actual = undef;
+    } elsif (/^Published (\d+) records to/) {
+        $actual = $1;
+    }
 
-	if (defined($expected) && defined($actual) && $expected != $actual) {
-		print "$table for $site: found $expected, published $actual\n";
-	}
+    if (defined($expected) && defined($actual) && $expected != $actual) {
+        print "$table for $site: found $expected, published $actual\n";
+    }
 }
